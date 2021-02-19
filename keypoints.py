0262d7a685700641ae5b8727a74ca94924b7c4d6
AntonJorg <anton2410@gmail.com>
2021-02-19 20:49:52 +0100

    Prototxt?

--- keypoints.py
+++ keypoints.py
@@ -38,14 +38,14 @@ for i in range(len()):
     x = (frameWidth * point[0]) / W
     y = (frameHeight * point[1]) / H
 
-    if prob &amp;amp;gt; threshold :
-        cv2.circle(frame, (int(x), int(y)), 15, (0, 255, 255), thickness=-1, lineType=cv.FILLED)
-        cv2.putText(frame, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3, lineType=cv2.LINE_AA)
-
-        # Add the point to the list if the probability is greater than the threshold
-        points.append((int(x), int(y)))
-    else :
-        points.append(None)
+#    if prob &amp;amp;gt; threshold :
+#        cv2.circle(frame, (int(x), int(y)), 15, (0, 255, 255), thickness=-1, lineType=cv.FILLED)
+#        cv2.putText(frame, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3, lineType=cv2.LINE_AA)
+#
+#        # Add the point to the list if the probability is greater than the threshold
+#        points.append((int(x), int(y)))
+#    else :
+#        points.append(None)
 
 cv2.imshow("Output-Keypoints",frame)
 cv2.waitKey(0)
